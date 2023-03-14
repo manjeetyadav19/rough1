f401675765c7fb0d6df35ed5416194adfaaa3962
manjeetyadav19 <83588426+manjeetyadav19@users.noreply.github.com>
2023-03-14 17:53:31 +0530

    Add files via upload

--- hadoop_search_older_files.sh
+++ hadoop_search_older_files.sh
@@ -4,12 +4,12 @@ PATH_NAME=""
 DAYS=""
 > hadoop_data
 
-hdfs dfs -ls -R / | grep "^-" | while read line ; do
+hdfs dfs -ls -R $PATH_NAME | grep "^-" | while read line ; do
 dir_date=$(echo ${line} | awk '{print $6}')
 difference=$(( ( ${today} - $(date -d ${dir_date} +%s) ) / ( 24*60*60 ) ))
 filePath=$(echo ${line} | awk '{print $8}')
 
-if [ ${difference} -gt 0 ]; then
+if [ ${difference} -gt $DAYS ]; then
     hdfs dfs -ls $filePath | awk '{print $6,$8}' >> hadoop_data
 fi
 done
